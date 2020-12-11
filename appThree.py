5a4df7d7fcd04b7e95361c21699a93d02e52cb11
Hanna <hannabowow@gmail.com>
2020-12-10 21:21:01 -0600

    Update appThree.py

--- appThree.py
+++ appThree.py
@@ -1,28 +1,28 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import cv2 as cv
+import cv2
 import streamlit.components.v1 as components
 #https://github.com/madhav727/medium/blob/master/finger_counting_video.py
 @st.cache()
 def skinmask(img):
-    hsvim = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    hsvim = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     lower = np.array([0, 48, 80], dtype = "uint8")
     upper = np.array([20, 255, 255], dtype = "uint8")
-    skinRegionHSV = cv.inRange(hsvim, lower, upper)
-    blurred = cv.blur(skinRegionHSV, (2,2))
-    ret, thresh = cv.threshold(blurred,0,255,cv.THRESH_BINARY)
+    skinRegionHSV = cv2.inRange(hsvim, lower, upper)
+    blurred = cv2.blur(skinRegionHSV, (2,2))
+    ret, thresh = cv2.threshold(blurred,0,255,cv2.THRESH_BINARY)
     return thresh
 @st.cache()
 def getcnthull(mask_img):
-    contours, hierarchy = cv.findContours(mask_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    contours = max(contours, key=lambda x: cv.contourArea(x))
-    hull = cv.convexHull(contours)
+    contours, hierarchy = cv2.findContours(mask_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = max(contours, key=lambda x: cv2.contourArea(x))
+    hull = cv2.convexHull(contours)
     return contours, hull
 @st.cache()
 def getdefects(contours):
-    hull = cv.convexHull(contours, returnPoints=False)
-    defects = cv.convexityDefects(contours, hull)
+    hull = cv2.convexHull(contours, returnPoints=False)
+    defects = cv2.convexityDefects(contours, hull)
     return defects
 st.title("ASL Translation Data Mining Project FALL2020")
 
@@ -40,14 +40,14 @@ st.subheader("Translation from live video to text: ")
 
 if st.button("Begin..."):
     st.success("Press Q to exit camera")
-    cap = cv.VideoCapture(0) # '0' for webcam
+    cap = cv2.VideoCapture(0) # '0' for webcam
     while cap.isOpened():
         _, img = cap.read()
         try:
             mask_img = skinmask(img)
             contours, hull = getcnthull(mask_img)
-            cv.drawContours(img, [contours], -1, (255,255,0), 2)
-            cv.drawContours(img, [hull], -1, (0, 255, 255), 2)
+            cv2.drawContours(img, [contours], -1, (255,255,0), 2)
+            cv2.drawContours(img, [hull], -1, (0, 255, 255), 2)
             defects = getdefects(contours)
             if defects is not None:
                 cnt = 0
@@ -65,11 +65,11 @@ if st.button("Begin..."):
                         cv.circle(img, far, 4, [0, 0, 255], -1)
                 if cnt > 0:
                     cnt = cnt+1
-                cv.putText(img, str(cnt), (0, 50), cv.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0) , 2, cv.LINE_AA)
-            cv.imshow("img", img)
+                cv2.putText(img, str(cnt), (0, 50), cv.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0) , 2, cv.LINE_AA)
+            cv2.imshow("img", img)
         except:
             pass
-        if cv.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
-    cv.destroyAllWindows()
+    cv2.destroyAllWindows()
